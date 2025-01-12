96fea66f608396ae479d365294b2b78894bd34b3
ikejimee <bethany.aung@gmail.com>
2025-01-13 05:15:52 +1100

    Update ausPhoneNumEmail.py
    Changes:
    - edited the regex to fit Australian numbers (including prefix of +614 and 04)
    - cleaned up code
    - testing
    
    TO DO:
    - documentation and more type strings
    - make code more simple

--- ausPhoneNumEmail.py
+++ ausPhoneNumEmail.py
@@ -5,46 +5,46 @@ import re
 import pyperclip
 from typing import List
 
-#allow +614 and just 04 as well
+
 
 PHONE_NUMBER_REGEX = re.compile(r'''(
-                            (\s*\d{3}|\(\d{3}\))                #area code (optional)
-                            (\s|\.|-)?                          #separator (optional)
-                            (\d{3})                             #first three digits
-                            (\.|-|\s)                           #second separator
-                            (\d{4})                             #last four digits
-
+                            (\+61|0)                            # Country code or leading zero 
+                            (\s|\.|-)?                          # Separator (optional)
+                            (4\d{2})                            # Two digits with leading 4
+                            (\.|-|\s)                           # Separator (optional)
+                            (\d{3})                             # Three digits
+                            (\.|-|\s)                           # Separator (optional)
+                            (\d{3})                             # Three digits
                               )''', re.VERBOSE)
 
-#TO DO:create email regex
 
 EMAIL_REGEX = re.compile(r'''(  
                         [a-zA-Z0-9._$+-]+                       #username
                         @                                       #symbol
                         [a-zA-Z0-9.-]+                          #domain name 
-                        (\.[a-zA-Z]{2,4})                       #dot-something        
-                      
+                        \.[a-zA-Z]{2,}                          #dot-something        
                         )''', re.VERBOSE)
-#TO DO: find matches in clipboard text
+
+ 
 
 def matchPattern(text: str) -> List[str]:
     matches = []
-    for groups in PHONE_NUMBER_REGEX.findall(text):
-        phoneNum = '-'.join([groups[1],groups[3],groups[5]])
-        matches.append(phoneNum)
-    for groups in EMAIL_REGEX.findall(text):
-        matches.append(groups[0])
-    return matches
+    for groups in PHONE_NUMBER_REGEX.findall(text): # Ensure the groups are not empty (only allow full patterns formed)
+        if groups[1:] is not None:
+            phone_number = ''.join([groups[1],groups[3],groups[5], groups[7]])
+            matches.append(phone_number)
+    for email_match in EMAIL_REGEX.findall(text):
+        matches.append(email_match)
+    return matches 
         
-    #TO DO: copy results to clipboard
 
 def copyToClipboard(matches: List[str]) -> None:
     if len(matches)>0:
         pyperclip.copy('\n'.join(matches))
-        print('Copied to clipboard:')
+        print('The following has been copied to clipboard:')
         print('\n'.join(matches))
     else:
-        print('No phone numbers or email addresses found')
+        print('There were no phone numbers or email addresses found')
   
   
 def main() -> None:
